c9c6697ec947985098c3bc8a4f7439748bb283d9
dmitry.senin <dmitry.senin@akvelon.com>
2019-05-05 15:49:05 +0300

    [Emacs + cmder] Emacs's client-server setup

---
+++ emacs-server-init.ps1
@@ -0,0 +1,7 @@
+# Path to startup file
+$startup_file = "$([Environment]::GetFolderPath('Startup'))\StartEmacsServer.bat"
+
+# Clean previous server file info first
+New-Item $startup_file -type file
+Set-Content -Path $startup_file -Value "del /Q ""%HOME%/.emacs.d/server/*"""
+Add-Content -Path $startup_file -Value "runemacs.exe --daemon"
