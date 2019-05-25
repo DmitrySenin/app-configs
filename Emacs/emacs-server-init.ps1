3b064fda7c4fb7790354ed0e9f06005e11b411b6
dmitry.senin <dmitry.senin@akvelon.com>
2019-05-25 22:52:39 +0300

    [Emacs] Workable Emacs Server setup

--- Emacs/emacs-server-init.ps1
+++ Emacs/emacs-server-init.ps1
@@ -3,5 +3,6 @@ $startup_file = "$([Environment]::GetFolderPath('Startup'))\StartEmacsServer.bat
 
 # Clean previous server file info first
 New-Item $startup_file -type file
-Set-Content -Path $startup_file -Value "del /Q ""%HOME%/.emacs.d/server/*"""
+Set-Content -Path $startup_file -Value "set HOME=%HomeDrive%%HomePath%"
+Add-Content -Path $startup_file -Value "del /Q ""%HOME%/.emacs.d/server/*"""
 Add-Content -Path $startup_file -Value "runemacs.exe --daemon"
--- Emacs/init.bat
+++ Emacs/init.bat
@@ -1,1 +1,6 @@
+rem this is for emacs.exe
 mklink /j "%userprofile%/.emacs.d" ".emacs.d"
+rem this is for emacsclientw.exe
+mklink /j "%appdata%/.emacs.d" ".emacs.d"
+
+powershell .\emacs-server-init.ps1
